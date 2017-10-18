59fcb6a3065bb7d5de72f69f045bd069f7c537ae
stoopfrench <32528005+stoopfrench@users.noreply.github.com>
2017-10-17 19:18:24 -0600

    initial commit

---
+++ main.js
@@ -0,0 +1,59 @@
+var students = [{
+ name: 'Liz',
+ age: 25,
+ city: 'Boulder'
+},{
+ name: 'Meghan',
+ age: 27,
+ city: 'Denver'
+},{
+ name: 'Trent',
+ age: 32,
+ city: 'Boulder'
+},{
+ name: 'Chelsea',
+ age: 24,
+ city: 'Boulder'
+},{
+ name: 'Amir',
+ age: 18,
+ city: 'Denver'
+}];
+
+
+// loop 3.1
+
+// for(var i = 0; i < students.length; i++) {
+
+// 	console.log(students[i].age);
+// }
+
+
+// loop 3.2
+
+// for(var i = 0; i < students.length; i++) {
+
+// 	console.log(students[i].name, students[i].city);
+// }
+
+
+// loop 3.3
+
+// for(var i = 0; i < students.length; i++) {
+
+// 	if (students[i].city === 'Boulder') {
+
+// 		console.log(students[i].name + ' is from ' + students[i].city);
+// 	}
+// }
+
+
+// loop 3.4
+
+// for (var i = 0; i < students.length; i++) {
+
+// 	if (students[i].age > 25) {
+
+// 		console.log(students[i].name + ' is older than 25');
+// 	}
+// }
